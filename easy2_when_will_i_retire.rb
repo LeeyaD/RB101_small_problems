81e1ad1a7491d96f93466e691e7ab4cf3d9ac31c
Leeya Davis <leeya463@gmail.com>
2019-09-02 11:30:04 -0400

    finish when will I retire

--- easy2_when_will_i_retire.rb
+++ easy2_when_will_i_retire.rb
@@ -29,9 +29,79 @@
 #  - Code (e.g. if conditionals)
 
 # ALGORITHM
-# - get user's CA and desired RA
-#   - validate CA & RA, to ensure it's not: a float; empty; stupidly high
-# - SUBPROCESS "figure out CY"
-# - SUBPROCESS "figure out RY"
-# - SUBPROCESS "figure out Y2R"
-# - output CY, RY and Y2R in sentences like in the example
+# - GET user's current age and SET it equal to 'current_age' <- ON FIRST LOOP
+#   - SUBPROCESS "validate to ensure it's not: a float; empty; stupidly high"
+#     - if 'current_age' is a float, emtpy or over 120 (oldest person 116) return false
+#       - 'current_age' is a float if there's a decimal & #s behind it "32" =~ /^\d*\.?\d*$/
+#       - 'current_age' is empty if it's "" or there's only space (i.e. "  ")
+#       - 'current_age' is stupidly high if it's over 120
+# - GET user's desired retirement age and SET it equal to 'retirement_age' <- ON SECOND LOOP
+#   - SUBPROCESS "validate to ensure it's not: a float; empty; stupidly high"
+# - GET for 'current_age' & 'retirement_age' can be extracted to a method
+# - use a #date/#time method to GET current year and SET to 'current year'
+# - SET 'years_to_retirement' equal to 'retirement_age' minus 'current_ age'
+# - SET 'retirement_year' equal to 'current_year' plus 'years_to_retirement'
+# - interpolate and output 'current_year', 'retirement_year', and 'years_to_retirement'
+# - in sentences like in the example/test case
+
+def valid_age?(age)
+  if /^\d*\.[1-9]$/.match?(age)
+    puts "Age must be a whole number. Try again."
+    return false
+  elsif age.to_i >= 130
+    puts "Let's not get crazy, even the oldest liviing person isn't that old. Try again."
+    return false
+  elsif age.strip.length == 0
+    puts "You must enter a number. Try again"
+    return false
+  else 
+    return true
+  end
+end
+
+def set_curr_age
+  loop do
+    print "How old are you? "
+    input = gets.chomp
+    return input if valid_age?(input)
+  end
+end
+
+def set_retire_age
+  loop do
+    print "At what age would you like to retire? "
+    input = gets.chomp
+    return input if valid_age?(input)
+  end
+end
+
+
+def this_year
+  t = Time.now
+  t.year
+end
+
+def yrs_to_retire(retire_age, curr_age)
+  retire_age.to_i - curr_age.to_i
+end
+
+def retire_yr(current_year, years_to_retirement)
+  current_year + years_to_retirement
+end
+
+puts "Let's see when you will retire!"
+  
+current_age = set_curr_age
+
+retirement_age = set_retire_age
+
+current_year = this_year
+
+years_to_retirement = yrs_to_retire(retirement_age, current_age)
+
+retirement_year = retire_yr(current_year, years_to_retirement)
+
+puts ""
+sleep(2)
+puts "It's #{current_year}. You will retire in #{retirement_year}."
+puts "You have only #{years_to_retirement} years of work to go!"
