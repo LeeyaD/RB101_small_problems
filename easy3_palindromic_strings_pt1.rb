7fa41e8761ad4ae685a66af75262543b077d90fd
Leeya Davis <leeya463@gmail.com>
2019-09-18 21:46:35 -0400

    readied next exercise

---
+++ easy3_palindromic_strings_pt1.rb
@@ -0,0 +1,16 @@
+# UNDERSTANDING THE PROBLEM - write a...method OR program?
+# Input
+
+# Output
+
+# Rules
+
+
+# EXAMPLES / TEST CASES
+
+
+# DATA STRUCTURE
+# Input: 
+# Rules as data: 
+
+# ALGORITHM
