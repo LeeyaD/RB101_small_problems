73bf5b72cf6a68df31f764a8449cb7a682e78929
Leeya Davis <leeya463@gmail.com>
2019-09-29 21:38:13 -0400

    understand the problem easy4 running totals

---
+++ easy4_running_totals.rb
@@ -0,0 +1,31 @@
+# UNDERSTANDING THE PROBLEM - write a method
+# Input
+# - Array (of Integers)
+# Output
+# - Array w/ same num of elements as original
+# Rules
+# - New Array must have the same number of elements as original
+# - New Array's elements but be a running total of original's elements
+
+# EXAMPLES / TEST CASES
+running_total([2, 5, 13]) == [2, 7, 20]
+running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+running_total([3]) == [3]
+running_total([]) == []
+# If orig. array is empty, a new empty array is returned
+
+
+# DATA STRUCTURE
+# Input: 
+# Rules as data: 
+
+# ALGORITHM
+# - 
+
+# CODE
+
+
+running_total([2, 5, 13]) == [2, 7, 20]
+running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+running_total([3]) == [3]
+running_total([]) == []
