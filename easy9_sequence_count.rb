8b4d1bc546b05dcaac087b92ad5c0e7c04984ab3
Leeya Davis <leeya463@gmail.com>
2019-12-10 19:33:10 -0500

    finish easy 9 sequence count

--- easy9_sequence_count.rb
+++ easy9_sequence_count.rb
@@ -6,7 +6,16 @@
 # You may assume that the count argument will always have a value of 0 or greater, while the starting number can
 # be any integer value. If the count is 0, an empty list should be returned.
 
-sequence(5, 1) == [1, 2, 3, 4, 5]
-sequence(4, -7) == [-7, -14, -21, -28]
-sequence(3, 0) == [0, 0, 0]
-sequence(0, 1000000) == []
+
+def sequence(count, number)
+  sequence = []
+  count.times do |idx|
+    sequence << number * (idx+1)
+  end
+  sequence
+end
+
+p sequence(5, 1) == [1, 2, 3, 4, 5]
+p sequence(4, -7) == [-7, -14, -21, -28]
+p sequence(3, 0) == [0, 0, 0]
+p sequence(0, 1000000) == []
