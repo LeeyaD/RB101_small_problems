16a28467e0bf533d0478b69ab1e99875dfc7f83a
Leeya Davis <leeya463@gmail.com>
2019-12-29 21:14:18 -0500

    finish medium 1 rotation part 1

---
+++ medium1_rotation_pt1.rb
@@ -0,0 +1,40 @@
+# THE PROBLEM
+
+# Input: an array
+# Output: a new 'rotated' array
+# Rules
+# - explicit:
+#   - 'rotated': moving the first element to the end of the array
+#   - can't use the method Array#rotate or Array#rotate!
+#   - original array should not be modified
+# - implicit:
+# Questions:
+# - how to handle an empty array?
+# - if dealing w/ a nested array, only rotate elements in top-most level?
+
+# EDGE CASES
+# none that I can think of...
+
+# DATA STRUCTURE
+# Input: an array
+# Rules Rep.: array, no logic needed
+
+# ALGO
+# clone given array to the variable 'new_array'
+# remove first element of 'new_array' with #delete_at(0), assigning the return to the variable 'rotated_element'
+# add 'rotated_element' to the end of 'new_array' w/ the shovel method, <<
+
+def rotate_array(array)
+  new_array = array.clone
+  rotated_element = new_array.delete_at(0)
+  new_array << rotated_element
+end
+
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+p x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+p x == [1, 2, 3, 4]                 # => true
